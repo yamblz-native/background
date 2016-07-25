28b150108b025b2d9bd06663ade52465c065b7ec
d954mas <d954mas@gmail.com>
2016-07-25 14:53:43 +0300

    recycleView collage

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -3,21 +3,20 @@ package ru.yandex.yamblz.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import java.util.List;
 
 import butterknife.BindView;
 import ru.yandex.yamblz.R;
 import ru.yandex.yamblz.artists.utils.DataSingleton;
-import ru.yandex.yamblz.loader.CollageLoaderManager;
+import ru.yandex.yamblz.ui.adapters.GenresAdapter;
 
 public class ContentFragment extends BaseFragment {
     @NonNull
-    @BindView(R.id.image_collage) ImageView collage;
+    @BindView(R.id.rv_genres) RecyclerView rv;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_content, container, false);
@@ -26,8 +25,8 @@ public class ContentFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        List<String> images= DataSingleton.get().getImagesForGenre("rock");
-        CollageLoaderManager.getLoader().loadCollage(images,collage,"rock");
-        CollageLoaderManager.getLoader().loadCollage(images,collage,"rock");
+        GenresAdapter genresAdapter=new GenresAdapter(DataSingleton.get().getGenres());
+        rv.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+        rv.setAdapter(genresAdapter);
     }
 }
