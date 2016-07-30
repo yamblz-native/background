c22716e0b82d42b82887aa670f62f28adbbb1da1
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2016-07-30 14:56:56 +0300

    Add bitmap caching
    Increase memory consumption  ~threefold
    Decrease CPU load ~tenfold

--- app/src/main/java/ru/yandex/yamblz/loader/DefaultCollageLoader.java
+++ app/src/main/java/ru/yandex/yamblz/loader/DefaultCollageLoader.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.LruCache;
 import android.widget.ImageView;
 
 import java.lang.ref.WeakReference;
@@ -19,16 +21,30 @@ import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
 import static android.os.Process.setThreadPriority;
 
 public class DefaultCollageLoader implements CollageLoader {
+    private static final String TAG = DefaultCollageLoader.class.getSimpleName();
+    private static ExecutorService executorService = Executors.newCachedThreadPool();
+
     private Resources resources;
+    private LruCache<Integer, Bitmap> bitmapCache;
 
     public DefaultCollageLoader(Resources resources) {
         this.resources = resources;
+
+        int maxCacheSize = (int) (Runtime.getRuntime().maxMemory() / 1024 / 2);
+        bitmapCache = new LruCache<Integer, Bitmap>(maxCacheSize) {
+            @Override
+            protected int sizeOf(Integer key, Bitmap bitmap) {
+                return bitmap.getByteCount() / 1024;
+            }
+        };
     }
+
 
     @Override
     public void loadCollage(int[] ids, ImageView imageView) {
         loadCollage(ids, imageView, null, null);
     }
+
 
     @Override
     public void loadCollage(int[] ids, ImageTarget imageTarget) {
@@ -36,11 +52,13 @@ public class DefaultCollageLoader implements CollageLoader {
 
     }
 
+
     @Override
     public void loadCollage(int[] ids, ImageView imageView, CollageStrategy collageStrategy) {
         loadCollage(ids, imageView, null, collageStrategy);
 
     }
+
 
     @Override
     public void loadCollage(int[] ids, ImageTarget imageTarget, CollageStrategy collageStrategy) {
@@ -56,25 +74,21 @@ public class DefaultCollageLoader implements CollageLoader {
         WeakReference<ImageView> refImageView = new WeakReference<>(iv);
         WeakReference<ImageTarget> refImageTarget = new WeakReference<>(it);
 
-        new AsyncCollageLoader(ids, resources, refImageView, refImageTarget, strategy).execute();
+        new AsyncCollageLoader(ids, refImageView, refImageTarget, strategy).execute();
     }
 
 
-    private static class AsyncCollageLoader extends AsyncTask<Void, Void, Bitmap> {
-        private static ExecutorService executorService = Executors.newCachedThreadPool();
-
+    private class AsyncCollageLoader extends AsyncTask<Void, Void, Bitmap> {
         private int[] ids;
-        private Resources resources;
         private WeakReference<ImageView> refImageView;
         private WeakReference<ImageTarget> refImageTarget;
         private CollageStrategy collageStrategy;
 
-        AsyncCollageLoader(int[] ids, Resources r, WeakReference<ImageView> iv, WeakReference<ImageTarget> it, CollageStrategy s) {
+        AsyncCollageLoader(int[] ids, WeakReference<ImageView> iv, WeakReference<ImageTarget> it, CollageStrategy strategy) {
             this.ids = ids;
-            this.resources = r;
             this.refImageView = iv;
             this.refImageTarget = it;
-            this.collageStrategy = s;
+            this.collageStrategy = strategy;
         }
 
 
@@ -89,22 +103,30 @@ public class DefaultCollageLoader implements CollageLoader {
         @Override
         protected Bitmap doInBackground(Void... params) {
             List<Future<Bitmap>> futures = new ArrayList<>(ids.length);
-            for (int id : ids) {
-                futures.add(executorService.submit(() -> {
-                    setThreadPriority(THREAD_PRIORITY_BACKGROUND);
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inTargetDensity = DisplayMetrics.DENSITY_MEDIUM;
-                    return BitmapFactory.decodeResource(resources, id, options);
-                }));
-            }
+            List<Bitmap> bitmaps = new ArrayList<>(ids.length);
 
-            List<Bitmap> bitmaps = new ArrayList<>(futures.size());
+            for (int id : ids) {
+                Bitmap bitmap = bitmapCache.get(id);
+
+                if (bitmap == null) {
+                    futures.add(executorService.submit(() -> {
+                        setThreadPriority(THREAD_PRIORITY_BACKGROUND);
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inTargetDensity = DisplayMetrics.DENSITY_MEDIUM;
+                        Bitmap bmp = BitmapFactory.decodeResource(resources, id, options);
+                        bitmapCache.put(id, bmp);
+                        return bmp;
+                    }));
+                } else {
+                    bitmaps.add(bitmap);
+                }
+            }
 
             for (Future<Bitmap> future : futures) {
                 try {
                     bitmaps.add(future.get());
                 } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
+                    Log.e(TAG, Log.getStackTraceString(e));
                 }
             }
 
