c9122f7c4de3c86ef11b6d06d17cd8b8d0382a75
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2016-08-01 14:49:32 +0300

    Handle delayed low priority tasks properly

--- app/src/main/java/ru/yandex/yamblz/handler/DefaultCriticalSectionsHandler.java
+++ app/src/main/java/ru/yandex/yamblz/handler/DefaultCriticalSectionsHandler.java
@@ -2,15 +2,20 @@ package ru.yandex.yamblz.handler;
 
 import android.os.Handler;
 
+import java.lang.ref.WeakReference;
 import java.util.Collections;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
+import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DefaultCriticalSectionsHandler implements CriticalSectionsHandler {
     private final Handler uiThreadHandler;
-    private final ConcurrentLinkedQueue<Task> tasks = new ConcurrentLinkedQueue<>();
+    private final Queue<Task> tasks = new ConcurrentLinkedQueue<>();
     private final Set<Integer> sections = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    private final Map<Task, WeakReference<Runnable>> futureTasks = Collections.synchronizedMap(new WeakHashMap<>());
 
     public DefaultCriticalSectionsHandler(Handler uiThreadHandler) {
         this.uiThreadHandler = uiThreadHandler;
@@ -51,19 +56,36 @@ public class DefaultCriticalSectionsHandler implements CriticalSectionsHandler {
 
     @Override
     public void postLowPriorityTaskDelayed(Task task, int delay) {
-        uiThreadHandler.postDelayed(() -> postLowPriorityTask(task), delay);
+        if (delay <= 0) {
+            postLowPriorityTask(task);
+            return;
+        }
+
+        Runnable futureTask = () -> postLowPriorityTask(task);
+        futureTasks.put(task, new WeakReference<>(futureTask));
+        uiThreadHandler.postDelayed(futureTask, delay);
     }
 
 
     @Override
     public void removeLowPriorityTask(Task task) {
         tasks.remove(task);
+
+        WeakReference<Runnable> refFutureTask = futureTasks.remove(task);
+        if (refFutureTask != null) {
+            Runnable futureTask = refFutureTask.get();
+            if (futureTask != null) {
+                uiThreadHandler.removeCallbacks(futureTask);
+            }
+        }
     }
 
 
     @Override
     public void removeLowPriorityTasks() {
-        tasks.clear();
+        for (Task task : tasks) {
+            removeLowPriorityTask(task);
+        }
     }
 
 
